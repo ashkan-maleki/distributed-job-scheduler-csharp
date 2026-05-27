aebfcb0ef775d2180927a0850cb2548c17a7902f
ma.maleki <ma.maleki@PC-1293>
2026-05-27 10:35:45 +0330

    feat(shared-domain): add ResultType as enum to have variation of result

--- src/Shared.Domain/DTOs/Result.cs
+++ src/Shared.Domain/DTOs/Result.cs
@@ -4,44 +4,74 @@ namespace Shared.Domain.DTOs;
 
 public class Result
 {
-    public static Result<T> Warn<T>(string message) => new(message);
-    public static Result<T> Fail<T>(string message) => new(message);
+    public static Result<T> Ok<T>(string message) => new(message, type: ResultType.Message);
+    public static Result<T> Fail<T>(string message) => new(message, type: ResultType.Error);
     public static Result<T> FromException<T>(Exception exception) => new(exception);
     public static Result<T> Ok<T>(T data) => new(data);
 }
 
+public enum ResultType
+{
+    Data,
+    Error,
+    Message,
+    Exception
+}
+
 public class Result<T>
 {
-    [MemberNotNullWhen(true, nameof(Data))]
-    public bool Ok => !(string.IsNullOrEmpty(Message) && _exception == null);
-    public T? Data { get; private set; }
-    public string Message { get; private set; }
-    private readonly Exception? _exception;
-    public Exception Exception => _exception ?? throw new InvalidOperationException();
+    private ResultType Type { get; }
 
-    private Result(string message, Exception? exception)
+    public T? Data { get; private set; }
+    public string? Message { get; private set; }
+    
+    public Exception? Exception { get; private set; }
+    
+    
+    private Result(string? message, Exception? exception, ResultType type)
     {
         Message = message;
-        _exception = exception;
+        Exception = exception;
+        Type = type;
     }
 
-    public Result(string message) => Message = message;
+    public Result(string message, ResultType type = ResultType.Error)
+    {
+        Message = message;
+        Exception = null;
+        Type = type;
+    }
 
     public Result(Exception exception)
     {
         Message = string.Empty;
-        _exception = exception;
+        Exception = exception;
+        Type = ResultType.Exception;
     }
 
     public Result(T data)
     {
         Message = string.Empty;
         Data = data;
+        Exception = null;
+        Type = ResultType.Data;
     }
-}
+    
+    [MemberNotNullWhen(true, nameof(Data))]
+    public bool HasData => (Data != null && Type == ResultType.Data);
+    
+    [MemberNotNullWhen(true, nameof(Message))]
+    public bool HasMessage => (!string.IsNullOrEmpty(Message) && Type == ResultType.Message);
+    
+    [MemberNotNullWhen(true, nameof(Message))]
+    public bool ErrorRaised => (!string.IsNullOrEmpty(Message) && Type == ResultType.Error);
 
-public static class ResultExtensions
-{
-    public static Result<T> ToResult<T>(this string message) => new Result<T>(message);
-    public static Result<T> ToResult<T>(this Exception exception) => new Result<T>(exception);
+    [MemberNotNullWhen(true, nameof(Exception))]
+    public bool ExceptionThrown => (Exception != null && Type == ResultType.Exception);
+    
+    
+    public bool Failed => (ExceptionThrown || ErrorRaised);
+    public bool Ok => !Failed;
+    
+    public Result<TNew> Copy<TNew>() => new(Message, Exception, type: Type);
 }
