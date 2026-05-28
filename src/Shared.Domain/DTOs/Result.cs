ab34f2a4e1ff32ef876aefed6d7086fbed2096ea
ma.maleki <ma.maleki@PC-1293>
2026-05-28 18:10:54 +0330

    feat(shared-domain): seperate result and query result

--- src/Shared.Domain/DTOs/Result.cs
+++ src/Shared.Domain/DTOs/Result.cs
@@ -4,104 +4,138 @@ namespace Shared.Domain.DTOs;
 
 public static class Results
 {
-    public static Result<T> Ok<T>(T data) => new(data);
-    public static Result<T> Saved<T>(string message) => new(message, status: ResultStatus.Saved);
-    public static Result<T> NotFound<T>(string message) => new(message, status: ResultStatus.NotFound);
-    public static Result<T> DomainErrorRaised<T>(string message) => new(message, ResultStatus.DomainError);
-    public static Result<T> ExceptionThrown<T>(Exception exception) => new(exception);
+    public static Result NoContent() => new Result(ResultStatus.NoContent);
+    public static Result Ok(string message) => new(message, status: ResultStatus.Ok);
+    public static Result NotFound(string message) => new(message, status: ResultStatus.NotFound);
+    public static Result DomainErrorRaised(string message) => new(message, ResultStatus.DomainError);
+    public static Result ExceptionThrown(Exception exception) => new(exception);
+}
+
+public static class QueryResults
+{
+    public static QueryResult<T> NoContent<T>() => Results.NoContent().ToQueryResult<T>();
+    public static QueryResult<T> Found<T>(T data) => new(data);
+    public static QueryResult<T> Ok<T>(string message) => Results.Ok(message).ToQueryResult<T>();
+    public static QueryResult<T> NotFound<T>(string message) => Results.NotFound(message).ToQueryResult<T>();
+
+    public static QueryResult<T> DomainErrorRaised<T>(string message) =>
+        Results.DomainErrorRaised(message).ToQueryResult<T>();
+
+    public static QueryResult<T> ExceptionThrown<T>(Exception exception) =>
+        Results.ExceptionThrown(exception).ToQueryResult<T>();
 }
 
 public enum ResultStatus
 {
     Ok,
+    Found,
     NoContent,
-    Saved,
     NotFound,
     DomainError,
     UnexpectedError
 }
 
-public class Result<T>
+public class Result
 {
-    private bool Success => _status is ResultStatus.Ok 
+    private bool Success => Status is ResultStatus.Found
         or ResultStatus.NoContent
-        or ResultStatus.Saved;
-    
-    private readonly ResultStatus _status;
+        or ResultStatus.Ok;
 
-    public T? Data { get; }
+    public ResultStatus Status { get; protected set; }
+
     public string? Message { get; }
-    
+
     public Exception? Exception { get; }
-    
-    
-    private Result(string? message, Exception? exception, ResultStatus status)
+
+
+    protected internal Result(ResultStatus status)
+    {
+        Message = string.Empty;
+        Exception = null;
+        Status = status;
+    }
+
+    protected Result(string? message, Exception? exception, ResultStatus status)
     {
         Message = message;
         Exception = exception;
-        _status = status;
-        
+        Status = status;
     }
 
-    public Result(string message, ResultStatus status = ResultStatus.NotFound)
+    protected internal Result(string message, ResultStatus status = ResultStatus.NotFound)
     {
         Message = message;
         Exception = null;
-        _status = status;
-        
+        Status = status;
     }
 
-    public Result(Exception exception)
+    protected internal Result(Exception exception)
     {
         Message = exception.Message;
         Exception = exception;
-        _status = ResultStatus.UnexpectedError;
+        Status = ResultStatus.UnexpectedError;
     }
 
-    public Result(T data)
-    {
-        Message = string.Empty;
-        Data = data;
-        Exception = null;
-        _status = ResultStatus.Ok;
-    }
-    
-    [MemberNotNullWhen(true, nameof(Data))]
-    public bool Ok => (Data is not null && _status == ResultStatus.Ok);
-    
     [MemberNotNullWhen(true, nameof(Exception))]
-    public bool ExceptionThrown => (Exception is not null && _status == ResultStatus.UnexpectedError);
+    public bool ExceptionThrown => (Exception is not null && Status == ResultStatus.UnexpectedError);
 
     [MemberNotNullWhen(true, nameof(Message))]
     public bool HasMessage => (!string.IsNullOrEmpty(Message) && Success);
 
     [MemberNotNullWhen(true, nameof(Message))]
     public bool ErrorRaised => (!string.IsNullOrEmpty(Message) && !Success && !ExceptionThrown);
-    
-    public bool TryOk([NotNullWhen(true)] out T? data)
-    {
-        data = Data;
-        return Ok;
-    }
 
-    public bool TryException([NotNullWhen(true)] out Exception? exception)
+    public bool TryGetException([NotNullWhen(true)] out Exception? exception)
     {
         exception = Exception;
         return ExceptionThrown;
     }
 
-    public bool TryMessage([NotNullWhen(true)] out string? message)
+    public bool TryGetMessage([NotNullWhen(true)] out string? message)
     {
         message = Message;
         return HasMessage;
     }
 
-    public bool TryError([NotNullWhen(true)] out string? message)
+    public bool TryGetError([NotNullWhen(true)] out string? message)
     {
         message = Message;
         return ErrorRaised;
     }
+}
+
+public class QueryResult<T> : Result
+{
+    public T? Data { get; }
+
+
+    protected internal QueryResult(string? message, Exception? exception, ResultStatus status)
+        : base(message, exception, status)
+    {
+    }
     
-    
-    public Result<TNew> SwapPayload<TNew>() => new(Message, Exception, status: _status);
+
+    protected internal QueryResult(T data) : base(ResultStatus.Found)
+    {
+        Data = data;
+    }
+
+    [MemberNotNullWhen(true, nameof(Data))]
+    public bool Found => (Data is not null && Status == ResultStatus.Found);
+
+
+    public bool TryGetData([NotNullWhen(true)] out T? data)
+    {
+        data = Data;
+        return Found;
+    }
+
+
+    public QueryResult<TNew> SwapPayload<TNew>() => new(Message, Exception, status: Status);
+}
+
+public static class QueryResultsExtensions
+{
+    public static QueryResult<T> ToQueryResult<T>(this Result result)
+        => new(result.Message, result.Exception, result.Status);
 }
