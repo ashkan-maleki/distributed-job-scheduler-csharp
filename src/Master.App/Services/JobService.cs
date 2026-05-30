f48d58a74d6bd305d20f077012c6df831d3407b9
ma.maleki <ma.maleki@PC-1293>
2026-05-30 22:00:42 +0330

    feat(master): enhance job service by using recently created result object

--- src/Master.App/Services/JobService.cs
+++ src/Master.App/Services/JobService.cs
@@ -10,23 +10,21 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
 {
     public async Task<List<Job>> AllAsync() => await jobRepository.AllAsync();
 
-    public async Task<QueryResult2<Job>> QueueJob(string name)
+    public async Task<IResult<Job>> QueueJob(string name)
     {
         Job job = new Job(name);
-        Result2 result2 = await jobRepository.AddAsync(job);
-        if (!result2.Ok)
-        {
-            return result2.ToQueryResult<Job>();
-        }
+        await jobRepository.AddAsync(job);
+        
         Exception? exception = await jobRepository.UnitOfWork.SaveEntitiesAsync();
         if (exception is not null)
         {
             return QueryResults.ExceptionThrown<Job>(exception);
         }
-        return QueryResults.Ok<Job>();
+
+        return new Ok<Job>(job);
     }
 
-    public async Task<QueryResult2<Job>> AssignJob(Guid workerId)
+    public async Task<IResult<Job>> AssignJob(Guid workerId)
     {
         QueryResult2<Job> jobQueryResult2 = await jobRepository.GetQueuedJobAsync();
         if (jobQueryResult2.NotFound)
@@ -54,7 +52,7 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
         return QueryResults.Found(job);
     }
 
-    public async Task<QueryResult2<Job>> StartJob(Guid jobId, Guid workerId)
+    public async Task<IResult<Job>> StartJob(Guid jobId, Guid workerId)
     {
         QueryResult2<Job> jobQueryResult2 = await jobRepository.GetAsync(jobId);
         if (jobQueryResult2.NotFound)
@@ -76,7 +74,7 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
         return QueryResults.Found(job);
     }
 
-    public async Task<QueryResult2<Job>> CompleteJob(Guid jobId, Guid workerId)
+    public async Task<IResult<Job>> CompleteJob(Guid jobId, Guid workerId)
     {
         QueryResult2<Job> jobQueryResult2 = await jobRepository.GetAsync(jobId);
         if (jobQueryResult2.NotFound)
@@ -85,8 +83,8 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
         }
         
         Job job = jobQueryResult2.Data;
-        Result2 result2 = job.Complete(workerId);
-        if (!result2.Ok)
+        IResult result = job.Complete(workerId);
+        if (result is Error error)
         {
             return result2.ToQueryResult<Job>();
         }
@@ -99,7 +97,7 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
         return QueryResults.Found(job);
     }
 
-    public async Task<QueryResult2<Job>> FailJob(Guid jobId, Guid workerId)
+    public async Task<IResult<Job>> FailJob(Guid jobId, Guid workerId)
     {
         QueryResult2<Job> jobQueryResult2 = await jobRepository.GetAsync(jobId);
         if (jobQueryResult2.NotFound)
@@ -108,8 +106,8 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
         }
         
         Job job = jobQueryResult2.Data;
-        Result2 result2 = job.Fail(workerId);
-        if (!result2.Ok)
+        IResult result = job.Fail(workerId);
+        if (result is Error error)
         {
             return result2.ToQueryResult<Job>();
         }
--- src/Master.Domain/Services/IJobService.cs
+++ src/Master.Domain/Services/IJobService.cs
@@ -5,10 +5,10 @@ namespace Master.Domain.Services;
 
 public interface IJobService
 {
-    public Task<QueryResult2<Job>> QueueJob(string name);
-    public Task<QueryResult2<Job>> AssignJob(Guid workerId);
-    public Task<QueryResult2<Job>> StartJob(Guid jobId, Guid workerId);
-    public Task<QueryResult2<Job>> CompleteJob(Guid jobId, Guid workerId);
-    public Task<QueryResult2<Job>> FailJob(Guid jobId, Guid workerId);
+    public Task<IResult<Job>> QueueJob(string name);
+    public Task<IResult<Job>> AssignJob(Guid workerId);
+    public Task<IResult<Job>> StartJob(Guid jobId, Guid workerId);
+    public Task<IResult<Job>> CompleteJob(Guid jobId, Guid workerId);
+    public Task<IResult<Job>> FailJob(Guid jobId, Guid workerId);
     public Task<List<Job>> AllAsync();
 }
