7bf8c83a17c9e47991acb17a4dfe3cf7bbdf7617
ma.maleki <ma.maleki@PC-1293>
2026-05-30 08:43:17 +0330

    feat(shared-domain): introduce a new result model for aggregate and entities

---
+++ src/Shared.Domain/DTOs/DomainResult.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Shared.Domain.DTOs;
+
+public enum DomainResultStatus
+{
+    Valid,
+    Invalid,
+}
+
+public class DomainResult
+{
+    private DomainResultStatus Status { get; set; }
+    private string? Message { get; }
+    
+    private DomainResult(string message)
+    {
+        Message = message;
+        Status = DomainResultStatus.Invalid;
+    }
+    
+    private DomainResult()
+    {
+        Status = DomainResultStatus.Valid;
+    }
+    
+    [MemberNotNullWhen(true, nameof(Message))]
+    public bool Invalid => Status == DomainResultStatus.Invalid;
+    public static DomainResult Ok() => new();
+    public static DomainResult Error(string message) => new(message);
+}
