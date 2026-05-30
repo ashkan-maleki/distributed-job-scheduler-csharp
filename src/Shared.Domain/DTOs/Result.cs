0f8a70d731e4c5bf60d09af646753738f40c037c
ma.maleki <ma.maleki@PC-1293>
2026-05-30 21:41:35 +0330

    feat(shared-domain): introduce a new result pattern

---
+++ src/Shared.Domain/DTOs/Result.cs
@@ -0,0 +1,57 @@
+﻿namespace Shared.Domain.DTOs;
+
+
+
+public interface IResult
+{
+    bool Success { get; }   
+    bool Failure { get; }
+}
+
+public interface IMessage : IResult
+{
+    string Content { get; }
+}
+
+public interface IResult<T> : IResult;
+
+public interface IMessage<T> : IResult<T>, IMessage;
+
+public interface IValue<T> : IResult<T>
+{
+    T Value { get; }
+}
+
+
+public abstract class Result(bool success) : IResult
+{
+    public virtual bool Success { get; } = success;
+    public virtual bool Failure => !Success;
+}
+
+
+
+public abstract class Message(bool success, string content) : Result(success), IMessage
+{
+    public string Content => content;
+}
+
+public class Success(string content) : Message(true, content);
+public class Error(string error) : Message(false, error);
+
+public abstract class Result<T>(bool success, T value) : Result(success), IValue<T>
+{
+    public T Value => value;
+}
+
+public abstract class Message<T>(bool success, string content): Result(success), IMessage<T>
+{
+    public string Content => content;
+}
+
+
+public class Ok<T>(T value) : Result<T>(true, value);
+public class Error<T>(string error) : Message<T>(false, error);
+
+
+public class Ok() : Result(true);
