f56e137ca988d69d4f3279713cd0a846fb013a53
ma.maleki <ma.maleki@PC-1293>
2026-06-08 22:28:33 +0330

    feat(master-domain): add id to desired state entity

---
+++ src/Master.Domain/Models/DesiredState.cs
@@ -0,0 +1,7 @@
+﻿namespace Master.Domain.Models;
+
+public class DesiredState(int desiredNumberOfWorkers)
+{
+    public int Id { get; set; }
+    public int DesiredNumberOfWorkers { get; set; } = desiredNumberOfWorkers;
+}
