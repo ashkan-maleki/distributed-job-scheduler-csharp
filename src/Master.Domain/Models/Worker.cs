628e8196bd79604a05bb9b466c63df77512ed220
ma.maleki <ma.maleki@PC-1293>
2026-06-08 22:37:45 +0330

    feat(master-domain): add RegisterdAt to keep the time of registration for worker

--- src/Master.Domain/Models/Worker.cs
+++ src/Master.Domain/Models/Worker.cs
@@ -8,13 +8,16 @@ public class Worker(string name, long version = 1)
     public string Name { get; } = name;
     public long Version { get; set; } = version;
     public WorkerState CurrentState { get; set; } =  WorkerState.Dead;
-    public DateTime HeartBeat { get; set; }
-    public bool HasHeartBeat => DateTime.Now - HeartBeat  < TimeSpan.FromSeconds(25);
+    public DateTime LastHeartBeat { get; set; }
+    public DateTime RegisteredAt { get; set; }
+    
+    public bool HasHeartBeat => DateTime.Now - LastHeartBeat  < TimeSpan.FromSeconds(25);
     public bool IsDead => CurrentState == WorkerState.Dead; 
     public bool IsRegistered => CurrentState == WorkerState.Registered;
 
     public void Register()
     {
+        RegisteredAt = DateTime.Now;
         CurrentState = WorkerState.Registered;
     }
     
@@ -26,7 +29,7 @@ public class Worker(string name, long version = 1)
     
     public void ReportHeartBeat()
     {
-        HeartBeat = DateTime.Now;
+        LastHeartBeat = DateTime.Now;
     }
 }
 
