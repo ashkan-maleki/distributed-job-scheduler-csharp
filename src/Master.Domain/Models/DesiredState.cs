3d08286f51e2b7eb35e1b2b83735bbcd265fc790
ma.maleki <ma.maleki@PC-1293>
2026-06-11 14:13:39 +0330

    feat(master-domain): ass a message for desired state to to send throud a message broker

--- src/Master.Domain/Models/DesiredState.cs
+++ src/Master.Domain/Models/DesiredState.cs
@@ -5,3 +5,5 @@ public class DesiredState(int desiredNumberOfWorkers)
     public int Id { get; set; }
     public int DesiredNumberOfWorkers { get; set; } = desiredNumberOfWorkers;
 }
+
+public record DesiredStateMessage(int DesiredNumberOfWorkers);
