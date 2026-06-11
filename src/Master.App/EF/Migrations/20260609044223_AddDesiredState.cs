852623beb965b9a826a7f802c0cc412fd928d5db
ma.maleki <ma.maleki@PC-1293>
2026-06-11 14:21:10 +0330

    feat(master-app): add desired state to our database

---
+++ src/Master.App/EF/Migrations/20260609044223_AddDesiredState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Master.App.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDesiredState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HeartBeat",
+                table: "Workers",
+                newName: "RegisteredAt");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastHeartBeat",
+                table: "Workers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "SchedulerStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DesiredNumberOfWorkers = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SchedulerStates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SchedulerStates");
+
+            migrationBuilder.DropColumn(
+                name: "LastHeartBeat",
+                table: "Workers");
+
+            migrationBuilder.RenameColumn(
+                name: "RegisteredAt",
+                table: "Workers",
+                newName: "HeartBeat");
+        }
+    }
+}
