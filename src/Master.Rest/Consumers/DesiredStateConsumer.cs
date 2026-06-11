f0e6f9ff9d716a3f821ac14d9f9c8f91ac18ea4e
ma.maleki <ma.maleki@PC-1293>
2026-06-11 14:22:25 +0330

    feat(master-api): add consumer for desired state

---
+++ src/Master.Rest/Consumers/DesiredStateConsumer.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit;
+using Master.Domain.Models;
+using Master.Domain.Services;
+
+namespace Master.Rest.Consumers;
+
+public class DesiredStateConsumer(IWorkerService workerService) : IConsumer<DesiredStateMessage>
+{
+    public async Task Consume(ConsumeContext<DesiredStateMessage> context) => await workerService.ScaleAsync(context.Message.DesiredNumberOfWorkers);
+}
