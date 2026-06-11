248450be9e1b9bc8819cf66fdb74b031c9dbb8ce
ma.maleki <ma.maleki@PC-1293>
2026-06-11 21:20:24 +0330

    feat(master-app): add a mechanism to remove workers after the completion of a job in case scale down requested

--- src/Master.App/Services/JobService.cs
+++ src/Master.App/Services/JobService.cs
@@ -6,7 +6,8 @@ using Shared.Domain.DTOs;
 
 namespace Master.App.Services;
 
-public class JobService(IJobRepository jobRepository, IWorkerRepository workerRepository) : IJobService
+public class JobService(IJobRepository jobRepository, IWorkerRepository workerRepository
+    , IDesiredStateService desiredStateService) : IJobService
 {
     public async Task<List<Job>> AllAsync() => await jobRepository.AllAsync();
 
@@ -71,13 +72,55 @@ public class JobService(IJobRepository jobRepository, IWorkerRepository workerRe
             async () => await jobRepository.GetAsync(jobId),
             (job, _) => job.Start(workerId));
 
+    
+    
+    private async Task<Result<Job>> ExecuteJobCompletionCommand(
+        Guid workerId,
+        Func<Task<Result<Job>>> loadJob,
+        Func<Job, Guid, IResult> jobStateTransition
+    )
+    {
+        Result<Worker> workerResult = await workerRepository.GetAsync(workerId);
+        if (workerResult.NotFound)
+        {
+            return workerResult.NotFoundResult;
+        }
+
+        Result<Job> jobResult = await loadJob();
+        if (jobResult.NotFound)
+        {
+            return jobResult.NotFoundResult;
+        }
+        
+        Job job = jobResult.OkResult.Value;
+        IResult result = jobStateTransition(job, workerId);
+        if (result is DomainFailure error)
+        {
+            return error;
+        }
+
+        if (await desiredStateService.DesiredNumberOfWorkersAsync() < await workerRepository.CountAsync())
+        {
+            workerRepository.Remove(workerResult.OkResult.Value);
+        }
+
+        result = await jobRepository.UnitOfWork.SaveEntitiesAsync();
+
+        if (result is CriticalError criticalError)
+        {
+            return criticalError;
+        }
+
+        return job;
+    }
+    
     public async Task<Result<Job>> CompleteJobAsync(Guid jobId, Guid workerId) =>
-        await ExecuteJobCommand(workerId,
+        await ExecuteJobCompletionCommand(workerId,
             async () => await jobRepository.GetAsync(jobId),
             (job, _) => job.Complete(workerId));
 
     public async Task<Result<Job>> FailJobAsync(Guid jobId, Guid workerId) =>
-        await ExecuteJobCommand(workerId,
+        await ExecuteJobCompletionCommand(workerId,
             async () => await jobRepository.GetAsync(jobId),
             (job, _) => job.Fail(workerId));
 }
