c4ef201e694048259b4b8168b40868cf08026f05
ma.maleki <ma.maleki@PC-1293>
2026-06-13 18:14:24 +0330

    feat(master): add services for managing the state of workers

---
+++ src/Master.Domain/Services/IWorkersStateService.cs
@@ -0,0 +1,11 @@
+﻿using Master.Domain.Models;
+using Shared.Domain.DTOs;
+
+namespace Master.Domain.Services;
+
+public interface IWorkersStateService
+{
+    bool RegistrationAllowed { get; }
+    Task<IResult> RegisterAsync();
+    Task<IResult> AddAsync(WorkersState workersState);
+}
