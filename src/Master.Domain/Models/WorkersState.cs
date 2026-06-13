9c2c49c4dab987100170913a0f7ff56a64466800
ma.maleki <ma.maleki@PC-1293>
2026-06-13 19:41:22 +0330

    feat(master-domain): add a constructor without parameters to make EF happy

--- src/Master.Domain/Models/WorkersState.cs
+++ src/Master.Domain/Models/WorkersState.cs
@@ -1,12 +1,25 @@
 ﻿namespace Master.Domain.Models;
 
-public class WorkersState(int desiredNumberOfWorkers, int currentNumberOfWorkers)
+public class WorkersState
 {
-    public int DesiredNumberOfWorkers { get; set; } = desiredNumberOfWorkers;
-    public int NumberOfWorkersToRegister { get; set; } = desiredNumberOfWorkers -  currentNumberOfWorkers;
+    private WorkersState()
+    {
+        
+    }
+
+    public WorkersState(int desiredNumberOfWorkers, int currentNumberOfWorkers)
+    {
+        DesiredNumberOfWorkers = desiredNumberOfWorkers;
+        NumberOfWorkersToRegister = desiredNumberOfWorkers - currentNumberOfWorkers;
+    }
+
+    public int Id { get; set; }
     
+    public int DesiredNumberOfWorkers { get; set; }
+    public int NumberOfWorkersToRegister { get; set; }
+
     public bool RegistrationAllowed => NumberOfWorkersToRegister > 0;
-    
+
 
     public void Register()
     {
