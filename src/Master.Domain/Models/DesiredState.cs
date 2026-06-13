004f4a6105de93fbcfd22475a1f0e8140bdb63f8
ma.maleki <ma.maleki@PC-1293>
2026-06-13 17:20:06 +0330

    feat(master-domain): roll back changes for DesiredState.cs

--- src/Master.Domain/Models/DesiredState.cs
+++ src/Master.Domain/Models/DesiredState.cs
@@ -1,25 +1,9 @@
 ﻿namespace Master.Domain.Models;
 
-public class DesiredState(int desiredNumberOfWorkers, int currentNumberOfWorkers)
+public class DesiredState(int desiredNumberOfWorkers)
 {
     public int Id { get; set; }
     public int DesiredNumberOfWorkers { get; set; } = desiredNumberOfWorkers;
-    public DateTime StartTime { get; set; } = DateTime.Now;
-    public DateTime EndTime { get; set; } = DateTime.MinValue;
-    
-    private int NumberOfWorkersToRegister { get; set; } = desiredNumberOfWorkers -  currentNumberOfWorkers;
-    
-    public bool AnyWorkersToRegister => NumberOfWorkersToRegister > 0;
-    
-
-    public void Register()
-    {
-        NumberOfWorkersToRegister--;
-        if (!AnyWorkersToRegister)
-        {
-            EndTime = DateTime.Now;
-        }
-    }
 }
 
 public record DesiredStateMessage(int DesiredNumberOfWorkers);
