0435c9878b75f78fc2ad7a10bd5a9d5783eb590a
ma.maleki <ma.maleki@PC-1293>
2026-06-13 17:55:00 +0330

    feat(master): add workers state repository to store and retrieve the state of workers

---
+++ src/Master.App/Repositories/WorkersStateRepository.cs
@@ -0,0 +1,26 @@
+﻿using Master.App.EF;
+using Master.Domain.Models;
+using Master.Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Shared.Domain.DTOs;
+using Shared.Domain.EF;
+
+namespace Master.App.Repositories;
+
+public class WorkersStateRepository(SchedulerDbContext dbContext) : IWorkersStateRepository
+{
+    public IUnitOfWork UnitOfWork { get; } = dbContext;
+    public async Task Add(WorkersState workersState) => _ = await dbContext.WorkersStates.AddAsync(workersState);
+
+    public async Task<Result<WorkersState>> Get()
+    {
+        WorkersState? workersState = await dbContext.WorkersStates
+            .Where(ws => ws.NumberOfWorkersToRegister > 0)
+            .FirstOrDefaultAsync();
+        if (workersState is null)
+        {
+            return new NotFound("Scaling the number of workers didn't take place");
+        }
+        return workersState;
+    }
+}
---
+++ src/Master.Domain/Repositories/IWorkersStateRepository.cs
@@ -0,0 +1,11 @@
+﻿using Master.Domain.Models;
+using Shared.Domain.Data;
+using Shared.Domain.DTOs;
+
+namespace Master.Domain.Repositories;
+
+public interface IWorkersStateRepository : IRepository
+{
+    public Task Add(WorkersState workersState);
+    public Task<Result<WorkersState>> Get();
+}
