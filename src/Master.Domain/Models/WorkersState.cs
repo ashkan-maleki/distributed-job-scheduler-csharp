e9fd1959fba95aef2838c78f43799da699adef81
ma.maleki <ma.maleki@PC-1293>
2026-06-13 17:52:53 +0330

    feat(master-domain): reposition WorkersState.cs from app to domain

---
+++ src/Master.Domain/Models/WorkersState.cs
@@ -0,0 +1,15 @@
+﻿namespace Master.Domain.Models;
+
+public class WorkersState(int desiredNumberOfWorkers, int currentNumberOfWorkers)
+{
+    public int DesiredNumberOfWorkers { get; set; } = desiredNumberOfWorkers;
+    public int NumberOfWorkersToRegister { get; set; } = desiredNumberOfWorkers -  currentNumberOfWorkers;
+    
+    public bool RegistrationAllowed => NumberOfWorkersToRegister > 0;
+    
+
+    public void Register()
+    {
+        NumberOfWorkersToRegister--;
+    }
+}
