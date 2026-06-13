6a861d8ca3c91cd305e88eb58d43bdba1c9f4842
ma.maleki <ma.maleki@PC-1293>
2026-06-13 19:36:50 +0330

    feat(master-app): add migrations for workers state

---
+++ src/Master.App/EF/Migrations/20260613155851_AddWorkersState.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Master.App.EF.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWorkersState : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WorkersStates",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DesiredNumberOfWorkers = table.Column<int>(type: "INTEGER", nullable: false),
+                    NumberOfWorkersToRegister = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkersStates", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WorkersStates");
+        }
+    }
+}
